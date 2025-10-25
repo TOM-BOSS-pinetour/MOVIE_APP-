3d7aa16269acfafd541ec55bcbef2289ec93ac0c
TOM-BOSS-pinetour <b94889340@gmail.com>
2025-10-25 21:51:46 +0800

    uptodat

--- src/app/_features/home/TopRatedMovieList.js
+++ src/app/_features/home/TopRatedMovieList.js
@@ -8,7 +8,7 @@ const ACCESS_TOKEN =
 
 export const TopRatedMovieList = () => {
   const [movies, setMovies] = useState([]);
-
+// 
   useEffect(() => {
     const fetchPopularMovies = async () => {
       try {
@@ -53,7 +53,7 @@ export const TopRatedMovieList = () => {
             </svg>
           </h3>
         </div>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 justify-center">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-5 gap-6 justify-center">
           {movies.map((movie) => (
             <MovieCard
               key={movie.id}
